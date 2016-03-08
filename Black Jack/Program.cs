8432dcc26325fc10c35876b6408bd2e3b25b8511
Kenneth Glawe <kenneth.glawe@gmail.com>
2016-03-08 12:31:19 +0100

    resetting dev.

--- Black Jack/Program.cs
+++ Black Jack/Program.cs
@@ -10,11 +10,7 @@ namespace Black_Jack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Heej");
-            Console.ReadKey();
-            //test
-            //test2
-            //testing
+            // TODO
         }
     }
 }
