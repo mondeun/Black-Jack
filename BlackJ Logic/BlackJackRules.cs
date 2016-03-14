5e854609eb154ebaf101d63294149c0eb05bcbe8
Kenneth Glawe <kenneth.glawe@gmail.com>
2016-03-14 10:54:31 +0100

    Fixed proper card value handling to rules

--- BlackJ Logic/BlackJackRules.cs
+++ BlackJ Logic/BlackJackRules.cs
@@ -21,16 +21,25 @@ namespace BlackJack.Players
 
         public static int GethandValue(Hand hand)
         {
-            //TODO: implement Ace values 1 and 11
-            //Blackjack specific rules for counting handvalue:
             var sum = 0;
             foreach (var card in hand.Cards)
             {
-                //Faces (J,Q,K) have value 10
-                if (card.Value > 10)
-                    sum += 10;
-                else
+                if (card.Value >= 2 && card.Value <= 10)
                     sum += card.Value;
+                // Faces (J, Q, K) have value 10
+                else if (card.Value > 10)
+                    sum += 10;
+
+                // Decide if an Ace is worth 1 or 11
+                if (card.Value == 1)
+                {
+                    // This gives Black Jack (sum = 21) 
+                    // and counts towards highest possible value
+                    if (sum <= 10)
+                        sum += 11;
+                    else
+                        sum += 1;
+                }
             }
             return sum;
         }
