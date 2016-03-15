3de91a3842fcd08ebd7cbc9c99bee7291a8e6c1f
Kenneth Glawe <kenneth.glawe@gmail.com>
2016-03-15 14:34:58 +0100

    Added the absolute bare bones of the game class

---
+++ Black Jack/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    class Game
+    {
+        private Blackjack blackjack;
+
+        public Game()
+        {
+            blackjack = new Blackjack(100);
+        }
+
+        /// <summary>
+        /// Play a game of Black Jack
+        /// </summary>
+        public void Run() { }
+    }
+}
